df64b92a8e938c47705a7de1976ee1224993898d
trm621 <t.maher621@gmail.com>
2022-01-05 20:03:01 -0500

    added more information to server

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+const path = require('path');
 const express = require('express');
+// const session = require('express-session');
+const exphbs = require('express-handlebars');
 // import sequelize connection-complete
 const sequelize = require('./config/connection');
 
@@ -7,6 +10,17 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+
+const helpers = require('./utils/helpers');
+
+const hbs = exphbs.create({ helpers });
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // turn on routes
 app.use(require('./controllers/'));
