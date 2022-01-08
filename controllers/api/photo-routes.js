359fd76ba964ea6aa6db5a14fe9ae20e3e65a552
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-01-08 09:24:09 -0500

    CRUD routes for photo

--- controllers/api/photo-routes.js
+++ controllers/api/photo-routes.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
-//const cloudinary = require("../utils/cloudinary");
-//const upload = require("./utils/multer");
+const cloudinary = require("../../utils/cloudinary");
+const upload = require("../../utils/multer");
 
 const { Photo } = require('../../models'); 
 
@@ -14,50 +14,50 @@ router.get('/', (req, res) => {
     });
 });
 
-// router.get("/", async (req, res) => {
-//     try {
-//       let photo = await Photo.find();
-//       res.json(photo);
-//     } catch (err) {
-//       console.log(err);
-//     }});
+router.get("/", async (req, res) => {
+    try {
+      let photo = await Photo.find();
+      res.json(photo);
+    } catch (err) {
+      console.log(err);
+    }});
 
-// router.put("/:id", upload.single("image"), async (req, res) => {
-//         try {
-//           let photo = await Photo.findById(req.params.id);
-//           // Delete image from cloudinary
-//           await cloudinary.uploader.destroy(photo.cloudinary_id);
-//           // Upload image to cloudinary
-//           const result = await cloudinary.uploader.upload(req.file.path);
-//           const data = {
-//             name: req.body.name || photo.name,
-//             photourl: result.secure_url || photo.photourl,
-//             cloudinary_id: result.public_id || photo.cloudinary_id,
-//           };
-//           photo = await Photo.findByIdAndUpdate(req.params.id, data, {
-//        new: true
-//        });
-//           res.json(photo);
-//         } catch (err) {
-//           console.log(err);
-//         }});
+router.put("/:id", upload.single("image"), async (req, res) => {
+        try {
+          let photo = await Photo.findById(req.params.id);
+          // Delete image from cloudinary
+          await cloudinary.uploader.destroy(photo.cloudinary_id);
+          // Upload image to cloudinary
+          const result = await cloudinary.uploader.upload(req.file.path);
+          const data = {
+            name: req.body.name || photo.name,
+            photourl: result.secure_url || photo.photourl,
+            cloudinary_id: result.public_id || photo.cloudinary_id,
+          };
+          photo = await Photo.findByIdAndUpdate(req.params.id, data, {
+       new: true
+       });
+          res.json(photo);
+        } catch (err) {
+          console.log(err);
+        }});
 
-// router.post("/", upload.single("image"), async (req, res) => {
-//   try {
-//     // Upload image to cloudinary
-//     const result = await cloudinary.uploader.upload(req.file.path);
-//      // Create new photo
-//     let photo = new Photo({
-//       name: req.body.name,
-//       photourl: result.secure_url,
-//       cloudinary_id: result.public_id,
-//     });
-//     // Save photo
-//     await photo.save();
-//     res.json(photo);
-//   } catch (err) {
-//     console.log(err);
-//   }}); 
+router.post("/", upload.single("image"), async (req, res) => {
+  try {
+    // Upload image to cloudinary
+    const result = await cloudinary.uploader.upload(req.file.path);
+     // Create new photo
+    let photo = new Photo({
+      name: req.body.name,
+      photourl: result.secure_url,
+      cloudinary_id: result.public_id,
+    });
+    // Save photo
+    await photo.save();
+    res.json(photo);
+  } catch (err) {
+    console.log(err);
+  }}); 
 
   router.delete("/:id", async (req, res) => {
     try {
--- utils/multer.js
+++ utils/multer.js
@@ -5,7 +5,8 @@ module.exports = multer({
   storage: multer.diskStorage({}),
   fileFilter: (req, file, cb) => {
     let ext = path.extname(file.originalname);
-      if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
+  // makes sure it is a picture file uploaded
+    if (ext !== ".jpg" && ext !== ".jpeg" && ext !== ".png") {
       cb(new Error("File type is not supported"), false);
       return;
     }
