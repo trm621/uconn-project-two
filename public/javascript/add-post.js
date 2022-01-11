c35f422dd2428176b77b2847e25fc66bd833ee78
trm621 <t.maher621@gmail.com>
2022-01-10 21:02:26 -0500

    everything is rendering

--- public/javascript/add-post.js
+++ public/javascript/add-post.js
@@ -2,13 +2,13 @@ async function newFormHandler(event) {
     event.preventDefault();
   
     const title = document.querySelector('input[name="post-title"]').value;
-    const post_url = document.querySelector('input[name="post-url"]').value;
+    const content = document.querySelector('input[name="content"]').value;
   
     const response = await fetch(`/api/posts`, {
       method: 'POST',
       body: JSON.stringify({
         title,
-        post_url
+        content
       }),
       headers: {
         'Content-Type': 'application/json'
@@ -22,4 +22,4 @@ async function newFormHandler(event) {
     }
   }
   
-  document.querySelector('.new-post-form').addEventListener('submit', newFormHandler);
+  document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
