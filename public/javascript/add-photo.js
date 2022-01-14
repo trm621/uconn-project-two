6d279dea2d898f95b4bf4e1c123f9bc9fe44e393
Dawn LeBlanc <dawnleblanc11@gmail.com>
2022-01-14 14:14:54 -0500

    photoupload

---
+++ public/javascript/add-photo.js
@@ -0,0 +1,27 @@
+async function newFormHandler(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('input[name="content"]').value;
+    const imageurl = document.querySelector('input[name="image"]');
+  
+    const response = await fetch(`/api/posts`, {
+      method: 'POST',
+      body: JSON.stringify({
+        title,
+        content,
+        imageurl
+      }),
+      headers: {
+        'Content-Type': 'application/json'
+      }
+    });
+  
+    if (response.ok) {
+      document.location.replace('/');
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document.querySelector('#new-post-form').addEventListener('submit', newFormHandler);
--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-const path = require('path');
-const express = require('express');
-const session = require('express-session');
-const exphbs = require('express-handlebars');
-const AWS = require('aws-sdk');
+const path = require("path");
+const fs = require("fs");
+const util = require("util");
+const unlinkFile = util.promisify(fs.unlink);
+const express = require("express");
+const session = require("express-session");
+const exphbs = require("express-handlebars");
+// specify the multer library
+const multer = require("multer");
+// place where all the files are uploaded in the server
+const upload = multer({ dest: "uploads/" });
+const { uploadFile, getFileStream } = require("./utils/s3");
+
 // e.log(err.stack)
 //    else{
 //      console.log(AWS.config.credentials.accessKeyId)
@@ -10,38 +18,64 @@ const AWS = require('aws-sdk');
 // })AWS.config.getCredentials(function(err){
 //   if (err) consol
 
-
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-const sequelize = require('./config/connection');
-const SequelizeStore = require('connect-session-sequelize')(session.Store);
+const sequelize = require("./config/connection");
+const SequelizeStore = require("connect-session-sequelize")(session.Store);
 
 const sess = {
-  secret: 'Super secret secret',
+  secret: "Super secret secret",
   cookie: {},
   resave: false,
   saveUninitialized: true,
   store: new SequelizeStore({
-    db: sequelize
-  })
+    db: sequelize,
+  }),
 };
 
 app.use(session(sess));
 
-const helpers = require('./utils/helpers');
+const helpers = require("./utils/helpers");
 
 const hbs = exphbs.create({ helpers });
 
-app.engine('handlebars', hbs.engine);
-app.set('view engine', 'handlebars');
+app.engine("handlebars", hbs.engine);
+app.set("view engine", "handlebars");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(express.static(path.join(__dirname, "public")));
 
-app.use(require('./controllers/'));
+app.use(require("./controllers/"));
+
+//  pulls the image from AWS- think we should use post id as the key???
+
+app.get("/images/:key", (req, res) => {
+  const key = req.params.key;
+  const readStream = getFileStream(key);
+
+  readStream.pipe(res);
+});
+
+// add asyncs and awaits
+app.post(
+  "/profile-upload-single",
+  upload.single("profile-file"),
+  (req, res) => {
+    // req.file is the `profile-file` file
+    // req.body will hold the other blog post fields
+    console.log(JSON.stringify(req.file));
+    var response = '<a href="/">Home</a><br>';
+    response += "Files uploaded successfully.<br>";
+    response += `<img src="${req.file.path}" /><br>`;
+    uploadFile(req.file.path);
+    // deletes the local file
+    unlinkFile(req.file.path);
+    return res.send(response);
+  }
+);
 
 sequelize.sync({ force: false }).then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
+  app.listen(PORT, () => console.log("Now listening"));
 });
